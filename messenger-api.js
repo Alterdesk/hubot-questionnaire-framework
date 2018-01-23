ad8b043c1d4b72181c8102a81559a40a70f25d34
jurriaan <jurriaan@alterdesk.com>
2018-01-23 10:57:20 +0100

    Added overriding of Robot.receive(), Answers and Listener classes and made response timeout, catch all and catch help configurable.

--- messenger-api.js
+++ messenger-api.js
@@ -1,9 +1,46 @@
+// Description:
+//   Messenger API helper script
+//
+// Dependencies:
+//   form-data
+//   http
+//   fs
+//
+// Configuration:
+//   OAuth token
+//
+// Commands:
+//
+// Author:
+//   Alterdesk
+
 // Requirements
 var FormData = require('form-data');
 var Http = require('http');
 var FileSystem = require('fs');
+const {TextMessage, LeaveMessage} = require('hubot');
+
+// Listeners for the bot
+var messengerBotListeners = {};
+
+var textRegex = new RegExp("\\w+", 'i');
+//var phoneRegex = new RegExp("/^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$", 'i');
+var phoneRegex = new RegExp("/^\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|3[875]\\d| 2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]| 4[987654310]|3[9643210]|2[70]|7|1)\\d{1,14}$", 'i');
+var emailRegex = new RegExp("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", 'i');
+var stopRegex;
+
+var responseTimeoutText;
+var responseTimeoutMs = 60000;
+var catchAllCommands = false;
+var catchAllText = "Command not found";
+var catchHelpCommand = false;
+var catchHelpText = "Help";
 
 module.exports = {
+
+    /*
+    *   Messenger API helper functions
+    */
 
     get: function(msg, apiUrl, getUrl, messengerToken, callback) {
       console.log("Messenger::get() >> " + getUrl);
@@ -79,5 +116,160 @@ module.exports = {
             }
           });
         });
+    },
+
+    // Override the default receiver
+    setRobotReceiver: function(robot) {
+      robot.defaultRobotReceiver = robot.receive;
+        robot.receive = function(message) {
+          var userId;
+          if(message.user != null) {
+            userId = message.user.id;
+          }
+          if(message instanceof TextMessage) {
+//              console.log("receive: " + message);
+              var lst;
+              if (userId != null && messengerBotListeners[userId] != null) {
+//                console.log("user: " + userId);
+                lst = messengerBotListeners[userId];
+                delete messengerBotListeners[userId];
+                if (lst.call(message)) {
+                  return;
+                }
+                // Put back to process next message
+                messengerBotListeners[userId] = lst;
+              }
+              if(catchHelpCommand && (message == robot.name + " help" || message == "help")) {  // TODO Maybe use regex
+//                console.log("Captured help");
+                var response = new robot.Response(robot, message, true);
+                response.send(catchHelpText);
+                return;
+              }
+              if(catchAllCommands && message != "uitnodigen" && message != "test") {
+                var response = new robot.Response(robot, message, true);
+                response.send(catchAllText);
+                return;
+              }
+          } else if(message instanceof LeaveMessage) {
+              console.log("Leave detected");
+              if(userId != null && messengerBotListeners[userId] != null) {
+                delete messengerBotListeners[userId];
+              }
+          }
+      //    console.log("Passing through original receive");
+          return robot.defaultRobotReceiver(message);
+        };
+    },
+
+    addListener: function(userId, listener) {
+      messengerBotListeners[userId] = listener;
+    },
+
+    removeListener: function(userId) {
+      delete messengerBotListeners[userId];
+    },
+
+    hasListener: function(userId) {
+      return messengerBotListeners[userId] != null;
+    },
+
+    getListener: function(userId) {
+      return messengerBotListeners[userId];
+    },
+
+    setStopRegex: function(s) {
+      stopRegex = s;
+    },
+
+    setResponseTimeoutText: function(t) {
+      responseTimeoutText = t;
+    },
+
+    setResponseTimeoutMs: function(ms) {
+      responseTimeoutMs = ms;
+    },
+
+    setCatchAll: function(catchAll) {
+      catchAllCommands = catchAll;
+    },
+
+    setCatchAllText: function(text) {
+      catchAllText = text;
+    },
+
+    setCatchHelp: function(catchHelp) {
+      catchHelpCommand = catchHelp;
+    },
+
+    setCatchHelpText: function(text) {
+      catchHelpText = text;
+    },
+
+    /*
+    *   Classes
+    */
+
+    // Data container for creating group chats
+    CreateGroupData: class {
+        constructor() {
+            this.allowContacts = true;
+            this.autoCloseAfter = 0;
+            this.autoExpireAfter = 0;
+            this.hybridMessaging = false;
+            this.membersCanInvite = false;
+        }
+    },
+
+    // Data container of answers that the user has given
+    Answers: class {
+    },
+
+    // Listener class for consecutive questions
+    Listener: class {
+      constructor(r, m, c, a, checkFor) {
+    //    console.info("constructor: robot: " + r + " message: " + m + " callback: " + c + " answers: " + a + " checkFor: " + checkFor);
+        this.call = this.call.bind(this);
+        this.robot = r;
+        this.callback = c;
+        this.answers = a;
+        if(checkFor == "text") {
+          this.regex = textRegex;
+        } else if(checkFor == "phoneNumber") {
+          this.regex = phoneRegex;
+        } else if(checkFor == "email") {
+          this.regex = emailRegex;
+        } else {
+          if (r.enableSlash) {
+            this.regex = new RegExp(`^(?:\/|${r.name}:?)\\s*(.*?)\\s*$`, 'i');
+          } else {
+            this.regex = new RegExp(`^${r.name}:?\\s*(.*?)\\s*$`, 'i');
+          }
+        }
+        this.matcher = (message) => {
+          if (message.text != null) {
+            return message.text.match(this.regex);
+          }
+        };
+        this.stopMatcher = (message) => {
+          if (message.text != null && stopRegex != null) {
+            return message.text.match(stopRegex);
+          }
+        };
+        this.timer = setTimeout(function () {
+            console.log("Response timeout: user: " + m.message.user.id);
+            delete messengerBotListeners[m.message.user.id];
+            if(responseTimeoutText != null) {
+              m.send(responseTimeoutText);
+            }
+        }, responseTimeoutMs);
+      }
+
+      call(message) {
+        clearTimeout(this.timer);
+        this.matches = this.matcher(message);
+        this.stop = this.stopMatcher(message);
+        this.callback(new this.robot.Response(this.robot, message, true), this);
+        return true;
+      }
     }
 }
