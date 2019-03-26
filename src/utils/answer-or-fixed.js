f6affa5ea6a5144b5e9a1bad357d42bede9a0d03
jurriaan <jurriaan@alterdesk.com>
2019-03-26 10:11:20 +0100

    AnswerOrFixed checks if data is Date before checking typeof object

--- src/utils/answer-or-fixed.js
+++ src/utils/answer-or-fixed.js
@@ -23,12 +23,14 @@ class AnswerOrFixed {
         var result = null;
         if(data instanceof AnswerOrFixed) {
             result = AnswerOrFixed.get(data.getValue(answers), answers);
+        } else if(data instanceof Date) {
+            result = data;
         } else if(typeof data === "object") {
-             result = "";
-             for(let index in data) {
-                 result += AnswerOrFixed.get(data[index], answers);
-             }
-         } else if(data != null) {
+            result = "";
+            for(let index in data) {
+                result += AnswerOrFixed.get(data[index], answers);
+            }
+        } else if(data != null) {
             result = data;
         }
         if(result == null) {
