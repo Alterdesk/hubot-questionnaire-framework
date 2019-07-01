49fec7456dea17785985e82007e72c2441f4a967
jurriaan <jurriaan@alterdesk.com>
2019-07-01 12:49:24 +0200

    RepeatFlowAction sets flow finished callback in constructor, minimal iterations can be set by answer, and current iteration value is saved to answers with "_iteration" suffix

--- src/actions/repeat-flow-action.js
+++ src/actions/repeat-flow-action.js
@@ -1,5 +1,6 @@
 const Action = require('./action.js');
 const AnswerCondition = require('./../conditions/answer-condition.js');
+const AnswerOrFixed = require('./../utils/answer-or-fixed.js');
 const Logger = require('./../logger.js');
 
 class RepeatFlowAction extends Action {
@@ -11,6 +12,14 @@ class RepeatFlowAction extends Action {
         this.conditions = [];
         this.iteration = -1;
         this.minIterations = 1;
+
+        if(this.repeatFlow) {
+            this.repeatFlow.finish((response, answers) => {
+                Logger.debug("RepeatFlowAction::finish() Iteration finished:", this.iteration);
+                this.askedQuestions = true;
+                this.checkRepeat();
+            });
+        }
     }
 
     start(response, answers, flowCallback) {
@@ -22,11 +31,6 @@ class RepeatFlowAction extends Action {
             this.flowCallback();
             return;
         }
-
-        this.repeatFlow.finish((response, answers) => {
-            Logger.debug("RepeatFlowAction::finish() Iteration finished:", this.iteration);
-            this.checkRepeat();
-        });
 
         this.checkRepeat();
     }
@@ -44,8 +48,13 @@ class RepeatFlowAction extends Action {
         }
         var checkIteration = this.iteration;
         checkIteration++;
-        if(checkIteration < this.minIterations) {
-            Logger.debug("RepeatFlowAction::checkRepeat() Minimal iterations not met:", this.minIterations);
+        var minIterations = AnswerOrFixed.get(this.minIterations, this.answers);
+        if(!minIterations) {
+            Logger.error("RepeatFlowAction::checkRepeat() Invalid minimal iterations given:", minIterations);
+            minIterations = 1;
+        }
+        if(checkIteration < minIterations) {
+            Logger.debug("RepeatFlowAction::checkRepeat() Minimal iterations not met:", minIterations);
             this.nextIteration();
             return;
         }
@@ -72,6 +81,9 @@ class RepeatFlowAction extends Action {
     nextIteration() {
         this.iteration++;
         Logger.debug("RepeatFlowAction::nextIteration() Iteration:", this.iteration);
+        if(this.repeatKey && this.repeatKey !== "") {
+            this.answers.add(this.repeatKey + "_iteration", this.iteration);
+        }
         this.repeatFlow.setRepeatIteration(this.iteration);
         this.flow.startSubFlow(this.repeatFlow, false);
     }
