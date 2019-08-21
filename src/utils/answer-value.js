a39f389016e08b372baeadb8cd4b946295b5c9f4
jurriaan <jurriaan@alterdesk.com>
2019-08-21 13:27:59 +0200

    Recursive calls to AnswerValue.get() fixed

--- src/utils/answer-value.js
+++ src/utils/answer-value.js
@@ -6,15 +6,16 @@ class AnswerValue {
         this.answerKey = answerKey
     }
 
-    getValue(answers, flow, repeatIteration) {
-        var answerKey = ChatTools.getAnswerKey(this.answerKey, flow, repeatIteration); // TODO Repeat iteration
+    getValue(answers, flow, forceRepeatIteration) {
+        var answerKey = ChatTools.getAnswerKey(this.answerKey, flow, forceRepeatIteration);
         return answers.get(answerKey);
     }
 
-    static get(data, answers, defaultValue, flow, repeatIteration) {
+    static get(data, answers, defaultValue, flow, forceRepeatIteration) {
         var result = null;
         if(data instanceof AnswerValue) {
-            result = AnswerValue.get(data.getValue(answers, flow, repeatIteration), answers);
+            var value = data.getValue(answers, flow, forceRepeatIteration);
+            result = AnswerValue.get(value, answers, defaultValue, flow, forceRepeatIteration);
         } else if(data instanceof Date) {
             result = data;
         } else if(typeof data === "object") {
