94c1ef0e6d046d28a9473ce8ee01b7480fd572cf
jurriaan <jurriaan@alterdesk.com>
2020-05-01 09:38:55 +0200

    Added RemoveGroupAction and added MessengerClient.removeGroupChat()

---
+++ src/actions/remove-group-action.js
@@ -0,0 +1,64 @@
+const Action = require('./action.js');
+const ChatTools = require('./../utils/chat-tools.js');
+const Logger = require('./../logger.js');
+
+class RemoveGroupAction extends Action {
+    constructor() {
+        super((flowCallback) => {
+            this.start(flowCallback);
+        }, 0);
+        this.isAux = false;
+    }
+
+    async start(flowCallback) {
+        if(!this.flow || !this.flow.msg || !this.flow.control) {
+            Logger.error("RemoveGroupAction::start() Invalid Flow or Control");
+            flowCallback();
+            return;
+        }
+        var answers = this.flow.answers;
+        var chatId;
+        var isAux;
+        if(this.chatId) {
+            chatId = this.getAnswerValue(this.chatId, answers);
+            isAux = this.getAnswerValue(this.isAux, answers);
+        } else {
+            var isGroup = ChatTools.isUserInGroup(this.flow.msg.message.user);
+            if(!isGroup) {
+                Logger.error("RemoveGroupAction::start() Not a group chat");
+                flowCallback();
+                return;
+            }
+            chatId = this.flow.msg.message.room;
+            isAux = false;
+        }
+        if(!chatId) {
+            Logger.error("RemoveGroupAction::start() Invalid destination chat id");
+            flowCallback();
+            return;
+        }
+
+        var sendEmail = this.getAnswerValue(this.sendEmail, answers, true);
+
+        await this.flow.control.messengerClient.removeGroupChat(chatId, isAux, sendEmail, this.overrideToken);
+        flowCallback();
+    }
+
+    setChatId(chatId) {
+        this.chatId = chatId;
+    }
+
+    setIsAux(isAux) {
+        this.isAux = isAux;
+    }
+
+    setSendEmail(sendEmail) {
+        this.sendEmail = sendEmail;
+    }
+
+    setOverrideToken(overrideToken) {
+        this.overrideToken = overrideToken;
+    }
+}
+
+module.exports = RemoveGroupAction;
